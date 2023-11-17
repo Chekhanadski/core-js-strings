b247e40be9864e7d81c29b8ca4046f7c2bd02dcc
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-11-17 23:24:54 +0100

    feat: solve task "sumOfCodes"

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -198,8 +198,18 @@ function removeLastOccurrences(str, value) {
  *   sumOfCodes('') => 0
  *   sumOfCodes() => 0
  */
-function sumOfCodes(/* str */) {
-  throw new Error('Not implemented');
+function sumOfCodes(str = '') {
+  if (str === '' || str === null || str === undefined) {
+    return 0;
+  }
+
+  let sum = 0;
+
+  for (let i = 0; i < str.length; i += 1) {
+    sum += str.charAt(i).charCodeAt();
+  }
+
+  return sum;
 }
 
 /**
